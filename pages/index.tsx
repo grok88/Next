042a30a800bc021b79c3e42feab807e59c427151
Alex <grok88@tut.by>
2021-09-19 22:20:45 +0300

    Add getStaticProps to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,11 @@ import {P} from '../components/P/P';
 import {Tag} from '../components/Tag/Tag';
 import {Rating} from '../components/Rating/Rating';
 import {withLayout} from '../layout/Layout';
-import {LearnGrid} from '../LearnGrid/LearnGrid';
+import {GetStaticProps} from 'next';
+import axios from 'axios';
+import {MenuItem} from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({menu, firstCategory}: HomeProps): JSX.Element {
     const [counter, setCounter] = useState<number>(0);
     const [rating, setRating] = useState<number>(4);
 
@@ -35,8 +37,28 @@ function Home(): JSX.Element {
                 <Tag size={'m'} color={'primary'}>Medium Tag primary</Tag>
             </div>
             <Rating rating={rating} setRating={setRating} isEditable/>
+            <ul>
+                {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+            </ul>
         </>
     );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const firstCategory = 0;
+
+    const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {firstCategory});
+    return {
+        props: {
+            menu,
+            firstCategory
+        }
+    };
+};
+
+interface HomeProps extends Record<string, unknown> {
+    menu: MenuItem[]
+    firstCategory: number
+}
